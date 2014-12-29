c823c2a3783fbafca4a2a22d82700beb82699c41
Dru Lang <drulang@gmail.com>
2014-12-29 17:39:02 -0500

    Using skaction

--- ZombieConga/ZombieConga/GameScene.m
+++ ZombieConga/ZombieConga/GameScene.m
@@ -8,6 +8,31 @@
 
 #import "GameScene.h"
 
+static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint CGPointSubtract(const CGPoint a, const CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a, const CGFloat scalar) {
+    return CGPointMake(a.x * scalar, a.y * scalar);
+}
+
+static inline CGFloat CGPointLength(const CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y); //Pythagorean Theorm
+}
+
+static inline CGPoint CGPointNormalize(const CGPoint a) {
+    CGFloat length = CGPointLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
+static inline CGFloat CGPointToAngle(const CGPoint a) {
+    return atan2f(a.y, a.x);
+}
+
 static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
 
 @implementation GameScene {
@@ -32,6 +57,7 @@ static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
         
         [self addChild:bg];
         [self addChild:_zombie];
+        [self spawnEnemy];
     }
     return self;
 }
@@ -43,30 +69,45 @@ static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
         _dt = 0;
     }
     _lastUpdateTime = currentTime;
-    NSLog(@"%0.2f milliseconds since last update", _dt * 1000);
-    
+    //NSLog(@"%0.2f milliseconds since last update", _dt * 1000);
     
     [self boundsCheckPlayer];
     [self rotateSprite:_zombie toFace:_velocity];
-    NSLog(@"Velocity: %@", NSStringFromCGPoint(_velocity));
+    //NSLog(@"Velocity: %@", NSStringFromCGPoint(_velocity));
     [self moveSprite:_zombie velocity:_velocity];
 }
 
+- (void)spawnEnemy {
+    SKSpriteNode *enemy = [SKSpriteNode spriteNodeWithImageNamed:@"enemy"];
+    enemy.position = CGPointMake(self.size.width + (enemy.size.width/2), self.size.height / 2);
+    [self addChild:enemy];
+    
+    SKAction *actionMidMove = [SKAction moveByX:-self.size.width / 2 - enemy.size.width /2 y:-self.size.height/2 + enemy.size.height / 2 duration:1];
+    SKAction *actionMove = [SKAction moveByX:-self.size.width /2 - enemy.size.width / 2 y:self.size.height/2 +enemy.size.height/2 duration:1];
+    SKAction *wait = [SKAction waitForDuration:.25];
+    SKAction *logMessage = [SKAction runBlock:^{
+        NSLog(@"Reached bottom");
+    }];
+
+    SKAction *sequence = [SKAction sequence:@[actionMidMove,logMessage, wait, actionMove]];
+    sequence = [SKAction sequence:@[sequence, [sequence reversedAction]]];
+    
+    SKAction *repeat = [SKAction repeatActionForever:sequence];
+    [enemy runAction:repeat];
+}
+
 - (void)moveSprite:(SKSpriteNode *)sprite velocity:(CGPoint)velocity {
-    CGPoint amountToMove = CGPointMake(velocity.x * _dt, _velocity.y * _dt); 
-    NSLog(@"Amount to move: %@", NSStringFromCGPoint(amountToMove));
+    CGPoint amountToMove = CGPointMultiplyScalar(_velocity, _dt);
     
-    sprite.position = CGPointMake(sprite.position.x + amountToMove.x, sprite.position.y + amountToMove.y);
-    
+    //NSLog(@"Amount to move: %@", NSStringFromCGPoint(amountToMove));
+    sprite.position = CGPointAdd(sprite.position, amountToMove);
 }
 
 - (void)moveZombieToward:(CGPoint)location {
-    CGPoint offset = CGPointMake(location.x - _zombie.position.x, location.y - _zombie.position.y);
-    CGFloat length = sqrtf(offset.x * offset.x + offset.y * offset.y); //Pythaogrean theorm
+    CGPoint offset = CGPointSubtract(location, _zombie.position);
     
-    CGPoint direction = CGPointMake(offset.x / length, offset.y / length); //Normalize to a unit vector
-    _velocity = CGPointMake(direction.x * ZOMBIE_MOVE_POINTS_PER_SEC, direction.y * ZOMBIE_MOVE_POINTS_PER_SEC);
-    
+    CGPoint direction = CGPointNormalize(offset);
+    _velocity = CGPointMultiplyScalar(direction, ZOMBIE_MOVE_POINTS_PER_SEC);
 }
 
 - (void)boundsCheckPlayer {
@@ -105,7 +146,7 @@ static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
 }
 
 - (void)rotateSprite:(SKSpriteNode *)sprite toFace:(CGPoint)direction{
-    sprite.zRotation = atan2f(direction.y, direction.x);
+    sprite.zRotation = CGPointToAngle(direction);
 }
 
 #pragma mark Touch Handling
